b67d2ca50945437edfcaa950f12d1a625361c036
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2026-01-12 09:31:12 -0500

    Uploaded code to folder

---
+++ swift/user-input/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  user-input
+//
+//  Created by Kyle Cartier on 1/12/26.
+//
+
+import Foundation
+
+// Prompt user for input and put it on the same line as the output the user enters
+
+print("Enter user input: ", terminator:"")
+
+// Read line of what the user is entering for input
+
+if let user_input = readLine()
+
+{
+ 
+    // Print the user's output that was entered into the prompt
+    
+    print("\(user_input)")
+    
+}
+
+// End of Code
